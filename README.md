bb8be0b9217fff8a28c673576ee69d43907b3f16
TimotheeGimbert <timothee.gimbert@free.fr>
2022-01-07 00:21:51 +0100

    shows winners name

---
+++ README.md
@@ -0,0 +1,3 @@
+RPG game from javascript console
+Perform basic attack on players,
+or spend some mana to perform special attacks !
--- index.js
+++ index.js
@@ -14,7 +14,12 @@ players.push(Grace, Ulder, Maana, Draven, Carl);
 const game = new Game();
 game.watchStats(players);
 
-while (game.turnsLeft > 0) {
+while (game.turnsLeft > 0 && game.playersLeft(players).length > 1) {
   const turn = new Turn(game, players);
   turn.startTurn();
 }
+
+console.log(">>>>>>>>>>>>>>>>>>>>>>>>> Winners are :");
+game.playersLeft(players).forEach(player => {
+  console.log(player.name);
+});
